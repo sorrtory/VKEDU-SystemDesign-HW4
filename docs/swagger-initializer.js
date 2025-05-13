ab038bf57f9ddfba76376da002043a0ba7d7d377
sorrtory <sorrtory@gmail.com>
2025-05-13 08:22:22 +0300

    update swagger

---
+++ docs/swagger-initializer.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    // url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [
+      { url: "yaml/wishlist.yaml", name: "Wishlist API" },
+      { url: "yaml/friendslist.yaml", name: "Friends list API" },
+      { url: "yaml/crowdfunding.yaml", name: "Crowdfunding API" }
+    ],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
